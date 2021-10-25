8a3210496e87bfdcd4d7485bed2fa15dd58d0060
sam boyer <sdboyer@grafana.com>
2021-10-25 05:03:21 -0400

    Add cue file embedder

---
+++ embed.go
@@ -0,0 +1,22 @@
+package scuemata
+
+import (
+	"embed"
+)
+
+// CueFS contains the raw .cue files that comprise the core scuemata system,
+// making them available directly in Go.
+//
+// This virtual filesystem is relied on by the Go functions exported by this
+// library, effectively co-versioning the Go logic with the CUE logic. It
+// is exported such that other Go packages have the unfettered capability to
+// create their own scuemata-based systems.
+//
+//go:embed *.cue
+var CueFS embed.FS
+
+// CueJointFS contains the raw scuemata .cue files, as well as the cue.mod
+// directory.
+//
+//go:embed *.cue cue.mod
+var CueJointFS embed.FS
