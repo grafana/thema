da58cc370f1391752c7b1608f0c464d3de6de6b7
sam boyer <sdboyer@grafana.com>
2021-12-13 23:08:45 -0500

    Significant refactors to surface, impl
    Also a first stab at ValidateCompatibilityInvariants, which is causing
    some nasty evaluator panics on relatively basic cases.

--- impl.go
+++ impl.go
@@ -4,52 +4,127 @@ import (
 	"cuelang.org/go/cue"
 )
 
-func BuildLineage(raw cue.Value) (Lineage, error) {
-	// TODO verify subsumption by #Lineage; renders many
-	// error checks below unnecessary
-	majiter, err := raw.LookupPath(cue.MakePath(cue.Str("Seqs"))).List()
-	if err != nil {
+func BuildLineage(raw cue.Value, lib Library) (Lineage, error) {
+	defLineage := lib.RawValue().LookupPath(cue.MakePath(cue.Def("#Lineage")))
+	if err := defLineage.Subsume(raw, cue.Raw()); err != nil {
 		return nil, err
 	}
 
-	var major int
-	var first, lastgvs *genericVersionedSchema
-	for majiter.Next() {
-		var minor int
-		miniter, _ := majiter.Value().List()
-		for miniter.Next() {
-			gvs := &genericVersionedSchema{
-				actual: miniter.Value(),
-				major:  major,
-				minor:  minor,
-				// This gets overwritten on all but the very final schema
-				translation: terminalTranslationFunc,
-			}
+	// seqiter, err := raw.LookupPath(cue.MakePath(cue.Str("Seqs"))).List()
+	// if err != nil {
+	// 	return nil, err
+	// }
 
-			if minor != 0 {
-				// TODO Verify that this schema is backwards compat with prior.
-				// Create an implicit translation operation on the prior schema.
-				lastgvs.translation = implicitTranslation(gvs.actual, gvs)
-				lastgvs.next = gvs
-			} else if major != 0 {
-				lastgvs.next = gvs
-				// x.0. There must exist a lens; load it up and ready it for
-				// use, and place it on the final schema in the prior sequence.
-				//
-				// Also...should at least try to make sure it's pointing at the
-				// expected schema, to maintain our invariants?
+	// var seqv int
+	// var first, lastgvs *genericVersionedSchema
+	// for seqiter.Next() {
+	// 	var schv int
+	// 	schiter, _ := seqiter.Value().LookupPath(cue.MakePath(cue.Str("schemas"))).List()
+	// 	for schiter.Next() {
+	// 		gvs := &genericVersionedSchema{
+	// 			actual: schiter.Value(),
+	// 			major:  seqv,
+	// 			minor:  schv,
+	// 			// This gets overwritten on all but the very final schema
+	// 			translation: terminalTranslationFunc,
+	// 		}
 
-				// TODO impl
-			} else {
-				first = gvs
-			}
-			lastgvs = gvs
-			minor++
-		}
-		major++
+	// 		if schv != 0 {
+	// 			// TODO Verify that this schema is backwards compat with prior.
+	// 			// Create an implicit translation operation on the prior schema.
+	// 			lastgvs.translation = implicitTranslation(gvs.actual, gvs)
+	// 			lastgvs.next = gvs
+	// 		} else if seqv != 0 {
+	// 			lastgvs.next = gvs
+	// 			// x.0. There must exist a lens; load it up and ready it for
+	// 			// use, and place it on the final schema in the prior sequence.
+	// 			//
+	// 			// Also...should at least try to make sure it's pointing at the
+	// 			// expected schema, to maintain our invariants?
+
+	// 			// TODO impl
+	// 		} else {
+	// 			first = gvs
+	// 		}
+	// 		lastgvs = gvs
+	// 		schv++
+	// 	}
+	// 	seqv++
+	// }
+
+	return &lineage{}, nil
+}
+
+type compatInvariantError struct {
+	rawlin    cue.Value
+	violation [2][2]int
+	detail    error
+}
+
+func (e *compatInvariantError) Error() string {
+	panic("TODO")
+}
+
+func ValidateCompatibilityInvariants(lin cue.Value, lib Library) error {
+	dlin := lib.RawValue().LookupPath(cue.MakePath(cue.Def("#Lineage")))
+	if dlin.Err() != nil {
+		panic(dlin.Err())
 	}
 
-	return first, nil
+	if err := dlin.Subsume(lin, cue.Raw(), cue.Schema()); err != nil {
+		return err
+	}
+
+	seqiter, _ := lin.LookupPath(cue.MakePath(cue.Str("Seqs"))).List()
+	var seqv int
+	var predecessor cue.Value
+	var predv [2]int
+	for seqiter.Next() {
+		var schv int
+		schemas := seqiter.Value().LookupPath(cue.MakePath(cue.Str("schemas")))
+		schiter, _ := schemas.List()
+		for schiter.Next() {
+			if schv == 0 && seqv == 0 {
+				// Very first schema, no predecessor to compare against
+				continue
+			}
+
+			sch := schiter.Value()
+			bcompat := sch.Subsume(predecessor, cue.Raw(), cue.Schema())
+			if (schv == 0 && bcompat == nil) || (schv != 0 && bcompat != nil) {
+				return &compatInvariantError{
+					rawlin:    lin,
+					violation: [2][2]int{predv, {seqv, schv}},
+					detail:    bcompat,
+				}
+			}
+
+			predecessor = sch
+			predv = [2]int{seqv, schv}
+			schv++
+		}
+		seqv++
+	}
+
+	return nil
+}
+
+type lineage struct {
+	name  string
+	first Schema
+	raw   cue.Value
+}
+
+func (lin *lineage) First() Schema {
+	return lin.first
+}
+
+func (lin *lineage) RawValue() cue.Value {
+	return lin.raw
+}
+
+func (lin *lineage) Name() string {
+	return lin.name
 }
 
 type genericVersionedSchema struct {
@@ -61,12 +136,12 @@ type genericVersionedSchema struct {
 }
 
 // Validate checks that the resource is correct with respect to the schema.
-func (gvs *genericVersionedSchema) Validate(r schema.Resource) error {
+func (gvs *genericVersionedSchema) Validate(r Resource) error {
 	name := r.Name
 	if name == "" {
 		name = "resource"
 	}
-	rv := ctx.CompileString(r.Value.(string), cue.Filename(name))
+	rv := gvs.actual.Context().CompileString(r.Value.(string), cue.Filename(name))
 	if rv.Err() != nil {
 		return rv.Err()
 	}
@@ -85,22 +160,18 @@ func (gvs *genericVersionedSchema) Version() (major int, minor int) {
 
 // Returns the next VersionedCueSchema
 func (gvs *genericVersionedSchema) Successor() Schema {
-	if gvs.next == nil {
-		// Untyped nil, allows `<sch> == nil` checks to work as people expect
-		return nil
-	}
-	return gvs.next
+	panic("TODO")
 }
 
 // Translate transforms a resource into a new Resource that is correct with
 // respect to its Successor schema.
-func (gvs *genericVersionedSchema) Translate(x schema.Resource) (schema.Resource, Schema, error) { // TODO restrict input/return type to concrete
+func (gvs *genericVersionedSchema) Translate(x Resource) (Resource, Schema, error) { // TODO restrict input/return type to concrete
 	r, sch, err := gvs.translation(x.Value)
 	if err != nil || sch == nil {
 		r = x.Value.(cue.Value)
 	}
 
-	return schema.Resource{Value: r}, sch, nil
+	return Resource{Value: r}, sch, nil
 }
 
 type translationFunc func(x interface{}) (cue.Value, Schema, error)
