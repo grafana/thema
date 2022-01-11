7347f34d103eef3ec287538b060bebcd2c268281
sam boyer <sdboyer@grafana.com>
2022-01-11 13:44:52 -0500

    Detour to fix exemplars pkg

--- exemplars/embed.go
+++ exemplars/embed.go
@@ -4,7 +4,7 @@ import (
 	"embed"
 )
 
-// CueFS contains the raw .cue files with all the thema exemplars.
+// cueFS contains the raw .cue files with all the thema exemplars.
 //
 //go:embed *.cue
-var CueFS embed.FS
+var cueFS embed.FS
