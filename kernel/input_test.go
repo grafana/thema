b39b7852ee219beaee604217da7544afd8168177
sam boyer <sdboyer@grafana.com>
2022-01-11 23:16:25 -0500

    Get input kernel convergence test working...sorta

--- kernel/input_test.go
+++ kernel/input_test.go
@@ -1,6 +1,7 @@
 package kernel
 
 import (
+	"fmt"
 	"testing"
 
 	"cuelang.org/go/cue/cuecontext"
@@ -20,10 +21,10 @@ type type10 struct {
 }
 
 var tf00 = func() interface{} {
-	return type00{}
+	return &type00{}
 }
 var tf10 = func() interface{} {
-	return type10{}
+	return &type10{}
 }
 
 func TestInputKernelInputs(t *testing.T) {
@@ -78,14 +79,14 @@ func TestInputKernelInputs(t *testing.T) {
 		cfg := InputKernelConfig{
 			Loader: jsondl,
 			TypeFactory: func() interface{} {
-				return &type00{}
+				return type00{}
 			},
 			Lineage: lin,
 			To:      thema.SV(0, 0),
 		}
 		_, err := NewInputKernel(cfg)
 		if err == nil {
-			t.Fatal("should fail when pointer type is emitted from type factory")
+			t.Fatal("should fail when non-pointer type is emitted from type factory")
 		}
 	})
 
@@ -108,4 +109,126 @@ func TestInputKernelInputs(t *testing.T) {
 			t.Fatal("should fail when type incompatible with schema is emitted from type factory")
 		}
 	})
+}
+
+func TestInputKernelConverge(t *testing.T) {
+	ctx := cuecontext.New()
+	lib := thema.NewLibrary(ctx)
+
+	lin, err := exemplars.RenameLineage(lib)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	k00, err := NewInputKernel(InputKernelConfig{
+		Loader:      jsondl,
+		TypeFactory: tf00,
+		Lineage:     lin,
+		To:          thema.SV(0, 0),
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	k10, err := NewInputKernel(InputKernelConfig{
+		Loader:      jsondl,
+		TypeFactory: tf10,
+		Lineage:     lin,
+		To:          thema.SV(1, 0),
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	tt := map[string]struct {
+		jsonstr  string
+		valid    bool
+		output00 type00
+		output10 type10
+	}{
+		// Commented cases fail due to what appear to be the same underlying bug
+		// with having the schema declared within lists
+		"malformed-json": {
+			jsonstr: `
+			{
+				foo": "bar"
+			}
+			`,
+			valid: false,
+		},
+		// "invalid": {
+		// 	jsonstr: `
+		// 	{
+		// 		"foo": "bar"
+		// 	}
+		// 	`,
+		// 	valid: false,
+		// },
+		"00good": {
+			jsonstr: `
+			{
+				"before": "renamedstr",
+				"unchanged": "unchanged str val"
+			}
+			`,
+			valid: true,
+			output00: type00{
+				Before:    "renamedstr",
+				Unchanged: "unchanged str val",
+			},
+			output10: type10{
+				After:     "renamedstr",
+				Unchanged: "unchanged str val",
+			},
+		},
+		// "10good": {
+		// 	jsonstr: `
+		// 	{
+		// 		"after": "renamedstr",
+		// 		"unchanged": "unchanged str val"
+		// 	}
+		// 	`,
+		// 	valid: true,
+		// 	output00: type00{
+		// 		Before:    "renamedstr",
+		// 		Unchanged: "unchanged str val",
+		// 	},
+		// 	output10: type10{
+		// 		After:     "renamedstr",
+		// 		Unchanged: "unchanged str val",
+		// 	},
+		// },
+	}
+
+	testfunc := func(t *testing.T, k InputKernel) {
+		to := k.Config().To
+		to00 := to == thema.SV(0, 0)
+		t.Run(fmt.Sprintf("to-%v", to), func(t *testing.T) {
+			for testname, tab := range tt {
+				t.Run(testname, func(t *testing.T) {
+					out, _, err := k.Converge([]byte(tab.jsonstr))
+					if !tab.valid {
+						if err == nil {
+							t.Fatal("should have failed to converge, but no err received")
+						}
+						return
+					} else if err != nil {
+						t.Fatal(err)
+					}
+
+					if to00 {
+						oval := *out.(*type00)
+						if tab.output00 != oval {
+							t.Fatalf("output targeting 0.0 was not as expected:\n\tWNT:%+v\n\tGOT:%+v\n", tab.output00, oval)
+						}
+					} else if tab.output10 != *out.(*type10) {
+						t.Fatalf("output targeting 1.0 was not as expected:\n\tWNT:%+v\n\tGOT:%+v\n", tab.output10, *out.(*type10))
+					}
+				})
+			}
+		})
+	}
+
+	testfunc(t, k00)
+	testfunc(t, k10)
 }
