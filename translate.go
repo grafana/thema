829976e7f6ad9ed45716067c180c8cec4fb4020e
sam boyer <sdboyer@grafana.com>
2022-01-11 23:01:00 -0500

    Move Instance into translate.go

--- translate.go
+++ translate.go
@@ -6,10 +6,50 @@ import (
 	"cuelang.org/go/cue"
 )
 
-// Translate takes an instance, known to be valid with respect to one schema in
-// a lineage, and transforms it to be valid with respect to another schema in
-// that lineage. A new instance is returned representing the transformed value,
-// along with any lacunae accumulated along the way.
+// An Instance represents some data that has been validated against a
+// lineage's schema. It includes a reference to the schema.
+type Instance struct {
+	// The CUE representation of the input data
+	raw cue.Value
+	// A name for the input data, primarily for use in error messages
+	name string
+	// The schema the data validated against/of which the input data is a valid instance
+	sch Schema
+}
+
+// AsSuccessor translates the instance into the form specified by the successor
+// schema.
+//
+// TODO figure out how to represent unary vs. composite lineages here
+func (i *Instance) AsSuccessor() (*Instance, TranslationLacunae) {
+	return i.Translate(i.sch.Successor().Version())
+}
+
+// AsPredecessor translates the instance into the form specified by the predecessor
+// schema.
+//
+// TODO figure out how to represent unary vs. composite lineages here
+func (i *Instance) AsPredecessor() (*Instance, TranslationLacunae) {
+	panic("TODO translation from newer to older schema is not yet implemented")
+}
+
+// UnwrapCUE returns the cue.Value representing the instance's underlying data.
+func (i *Instance) UnwrapCUE() cue.Value {
+	return i.raw
+}
+
+// Schema returns the schema which subsumes/validated this instance.
+func (i *Instance) Schema() Schema {
+	return i.sch
+}
+
+func (i *Instance) lib() Library {
+	return getLinLib(i.Schema().Lineage())
+}
+
+// Translate transforms the Instance so that it's an instance of another schema
+// in the lineage. A new *Instance is returned representing the transformed
+// value, along with any lacunae accumulated along the way.
 //
 // Forward translation within a sequence (e.g. [0, 0] to [0, 7]) is trivial, as
 // all those schema changes are established as backwards compatible by Thema's
@@ -26,23 +66,23 @@ import (
 // Thema does not guarantee that translating from [0, 0] to [1, 0] and back to
 // [0, 0] will result in the exact original input.
 //
-// NOTE reverse translation s not yet supported, and attempting it will panic.
+// NOTE reverse translation is not yet supported, and attempting it will panic.
 //
 // TODO define this in terms of Instance.AsSuccessor/AsPredecessor, rather than
 // those in terms of this.
-func Translate(inst *Instance, to SyntacticVersion) (*Instance, TranslationLacunae) {
-	if to.less(inst.Schema().Version()) {
+func (i *Instance) Translate(to SyntacticVersion) (*Instance, TranslationLacunae) {
+	if to.less(i.Schema().Version()) {
 		panic("TODO translation from newer to older schema is not yet implemented")
 	}
-	newsch, err := Pick(inst.Schema().Lineage(), to)
+	newsch, err := i.Schema().Lineage().Schema(to)
 	if err != nil {
 		panic(fmt.Sprintf("no schema in lineage with version %v, cannot translate", to))
 	}
 
 	out, err := cueArgs{
-		"linst": inst.asLinkedInstance(),
+		"linst": i.asLinkedInstance(),
 		"to":    to,
-	}.call("#Translate", inst.lib())
+	}.call("#Translate", i.lib())
 	if err != nil {
 		// This can't happen without a name change or an invariant violation
 		panic(err)
@@ -53,7 +93,7 @@ func Translate(inst *Instance, to SyntacticVersion) (*Instance, TranslationLacun
 
 	return &Instance{
 		raw:  out.LookupPath(cue.MakePath(cue.Str("linst"), cue.Str("inst"))),
-		name: inst.name,
+		name: i.name,
 		sch:  newsch,
 	}, lac
 }
