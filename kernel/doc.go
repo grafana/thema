691884324f27372c87fc5da3b544de1695bd84b2
sam boyer <sdboyer@grafana.com>
2022-09-28 15:16:23 -0400

    Remove kernel package

--- kernel/doc.go
+++
@@ -1,6 +0,0 @@
-// Package kernel contains Thema kernels, which compose common patterns
-// for relating Thema-schematized data to a Go program.
-//
-// Each kernel covers a distinct behavioral pattern. All kernels rely solely on
-// the public interface of the main thema Go package.
-package kernel
--- kernel/types.go
+++
@@ -1,7 +0,0 @@
-package kernel
-
-// A TypeFactory must emit a pointer to the Go type that a kernel will
-// ultimately produce as output.
-//
-// TODO the function accomplished by this should be trivial to achieve with generics...?
-type TypeFactory func() interface{}
