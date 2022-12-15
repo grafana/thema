d4c7d24c7f2107dd11ea998cdcc9208c7a0eaea7
sam boyer <sdboyer@grafana.com>
2022-12-15 16:33:40 -0500

    openapi: Total overhaul of codegen
    Refactor codegen to no longer rely on eval and printing, taking
    advantage of the ability to pass a cue.Value to the CUE standard library
    package.
    
    Also introduce support for a group flag, which allows translating only the
    children of the root schema to OpenAPI.
    
    And introduce a long-overdue test txtar-based test framework.

--- encoding/openapi/oapi_test.go
+++ encoding/openapi/oapi_test.go
@@ -6,20 +6,21 @@ import (
 	"cuelang.org/go/cue/cuecontext"
 	"cuelang.org/go/cue/errors"
 	"cuelang.org/go/cue/format"
+	"github.com/google/go-cmp/cmp"
 	"github.com/grafana/thema"
 	"github.com/grafana/thema/exemplars"
+	cuetxtar "github.com/grafana/thema/internal/txtartest"
 )
 
 // TODO make a testscript-based golden file approach to this that captures all exemplar encoding
-func testGenerateSchema(t *testing.T) {
+func TestGenerateSchema(t *testing.T) {
 	rt := thema.NewRuntime(cuecontext.New())
 	lin, err := exemplars.ExpandLineage(rt)
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	sch := thema.SchemaP(lin, thema.SV(0, 0))
-	f, err := GenerateSchema(sch, nil)
+	f, err := GenerateSchema(lin.First(), nil)
 	if err != nil {
 		t.Fatal(errors.Details(err, nil))
 	}
@@ -28,6 +29,48 @@ func testGenerateSchema(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-
-	_ = b
+	if string(b) != want {
+		t.Fatal(cmp.Diff(string(b), want))
+	}
 }
+
+func TestGenerate(t *testing.T) {
+	test := cuetxtar.CueTest{
+		Root: "./testdata",
+		Name: "generate",
+	}
+
+	test.Run(t, func(t *cuetxtar.Test) {
+		lin := t.BindLineage(nil)
+
+		f, err := GenerateSchema(lin.Latest(), nil)
+		if err != nil {
+			// FIXME we should probably accrue errors
+			t.WriteErrors(errors.Promote(err, ""))
+		} else {
+			f.Filename = "base.json"
+			t.WriteFile(f)
+		}
+	})
+}
+
+var want = `"openapi": "3.0.0"
+"info": {
+	"title":   "expand"
+	"version": "0.0"
+}
+"paths": {}
+"components": {
+	"schemas": {
+		"expand": {
+			"type": "object"
+			"required": ["init"]
+			"properties": {
+				"init": {
+					"type": "string"
+				}
+			}
+		}
+	}
+}
+`
