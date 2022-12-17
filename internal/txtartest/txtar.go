8aff8b93dc1b34c78940f0ba706db462000ed857
sam boyer <sdboyer@grafana.com>
2022-12-17 00:44:30 -0500

    Add import and embedding test case

--- internal/txtartest/txtar.go
+++ internal/txtartest/txtar.go
@@ -619,6 +619,10 @@ func (x *CueTest) Run(t *testing.T, f func(tc *Test)) {
 
 			if update {
 				sort.Slice(a.Files, func(i, j int) bool {
+					isout := func(s string) bool { return strings.HasPrefix(s, "out/")}
+					if isout(a.Files[i].Name) != isout(a.Files[j].Name) {
+						return isout(a.Files[j].Name)
+					}
 					return a.Files[i].Name < a.Files[j].Name
 				})
 
