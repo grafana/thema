d3e4be30442d89bf946cbdbef78840f3256cd955
Austin Pond <austin.pond@grafana.com>
2023-04-12 17:09:31 -0400

    Fork deepmap/oapi-codegen into internal/deepmap/oapi-codegen. Include only pkg/codegen and pkg/util, which thema's go codegen utilizes, and made changes to pkg/codegen based on the unmerged deepmap/oapi-codegen PR (https://github.com/deepmap/oapi-codegen/pull/717) to fix bug where thema go codegen creates empty interfaces for lineages with joinSchema or any joined (&) CUE values.

---
+++ internal/deepmap/oapi-codegen/pkg/codegen/filter_test.go
@@ -0,0 +1,69 @@
+package codegen
+
+import (
+	"testing"
+
+	"github.com/getkin/kin-openapi/openapi3"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestFilterOperationsByTag(t *testing.T) {
+	packageName := "testswagger"
+	t.Run("include tags", func(t *testing.T) {
+		opts := Configuration{
+			PackageName: packageName,
+			Generate: GenerateOptions{
+				EchoServer:   true,
+				Client:       true,
+				Models:       true,
+				EmbeddedSpec: true,
+			},
+			OutputOptions: OutputOptions{
+				IncludeTags: []string{"hippo", "giraffe", "cat"},
+			},
+		}
+
+		loader := openapi3.NewLoader()
+		loader.IsExternalRefsAllowed = true
+
+		// Get a spec from the test definition in this file:
+		swagger, err := loader.LoadFromData([]byte(testOpenAPIDefinition))
+		assert.NoError(t, err)
+
+		// Run our code generation:
+		code, err := Generate(swagger, opts)
+		assert.NoError(t, err)
+		assert.NotEmpty(t, code)
+		assert.NotContains(t, code, `"/test/:name"`)
+		assert.Contains(t, code, `"/cat"`)
+	})
+
+	t.Run("exclude tags", func(t *testing.T) {
+		opts := Configuration{
+			PackageName: packageName,
+			Generate: GenerateOptions{
+				EchoServer:   true,
+				Client:       true,
+				Models:       true,
+				EmbeddedSpec: true,
+			},
+			OutputOptions: OutputOptions{
+				ExcludeTags: []string{"hippo", "giraffe", "cat"},
+			},
+		}
+
+		loader := openapi3.NewLoader()
+		loader.IsExternalRefsAllowed = true
+
+		// Get a spec from the test definition in this file:
+		swagger, err := loader.LoadFromData([]byte(testOpenAPIDefinition))
+		assert.NoError(t, err)
+
+		// Run our code generation:
+		code, err := Generate(swagger, opts)
+		assert.NoError(t, err)
+		assert.NotEmpty(t, code)
+		assert.Contains(t, code, `"/test/:name"`)
+		assert.NotContains(t, code, `"/cat"`)
+	})
+}
