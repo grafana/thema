42dd43319596f31c01d758d903c87663673c5bfb
Joan López de la Franca Beltran <joanjan14@gmail.com>
2023-05-11 02:00:21 +0200

    thema: Refine Translate() tests

--- instance_test.go
+++ instance_test.go
@@ -1,14 +1,14 @@
-package thema_test
+package thema
 
 import (
+	"cuelang.org/go/cue"
+	"encoding/json"
 	"fmt"
+	"io"
 	"strings"
 	"testing"
 
-	"github.com/grafana/thema"
-	"github.com/grafana/thema/internal/txtartest/bindlin"
 	"github.com/grafana/thema/internal/txtartest/vanilla"
-	"github.com/grafana/thema/vmux"
 
 	"cuelang.org/go/cue/cuecontext"
 	"github.com/stretchr/testify/require"
@@ -21,41 +21,60 @@ func TestInstance_Translate(t *testing.T) {
 	}
 
 	ctx := cuecontext.New()
-	rt := thema.NewRuntime(ctx)
+	rt := NewRuntime(ctx)
 
 	test.Run(t, func(tc *vanilla.Test) {
 		if !tc.HasTag("multiversion") {
 			return
 		}
 
-		tName := strings.Replace(tc.Name(), t.Name()+"/", "", -1)
-
-		lin, lerr := bindlin.BindTxtarLineage(tc, rt)
+		lin, lerr := bindTxtarLineage(tc, rt)
 		require.NoError(tc, lerr)
 
 		for sch := lin.First(); sch != nil; sch = sch.Successor() {
-			for name, ex := range sch.Examples() {
+			for _, example := range sch.Examples() {
 				for sch := lin.First(); sch != nil; sch = sch.Successor() {
-					// TODO: Validate lacunas
 					to := sch.Version()
-					tinst, _ := ex.Translate(to)
+					tinst, lacunas := example.Translate(to)
 					require.NotNil(t, tinst)
 
-					raw := tinst.Underlying()
-					require.True(t, raw.Exists())
-					require.NoError(t, raw.Err())
+					result := tinst.Underlying()
+					require.True(t, result.Exists())
+					require.NoError(t, result.Err())
 
-					codec := vmux.NewJSONCodec("test")
-					rawBytes, err := codec.Encode(raw)
-					require.NoError(t, err)
-
-					wName := fmt.Sprintf("%s-%s-%s->%s.json", tName, name, ex.Schema().Version().String(), to.String())
-					w := tc.Writer(wName)
-					_, err = w.Write(rawBytes)
-					require.NoError(t, err)
+					writeGolden(tc, to, example, result, lacunas)
 				}
 			}
 		}
-
 	})
+}
+
+func writeGolden(tc *vanilla.Test, to SyntacticVersion, example *Instance, result cue.Value, lacunas TranslationLacunas) {
+	tc.Helper()
+
+	fromStr := example.Schema().Version().String()
+	toStr := to.String()
+
+	exName := example.name
+	tName := strings.Replace(tc.Name(), tc.Name()+"/", "", -1)
+	wName := fmt.Sprintf("%s-%s-%s->%s.json", tName, fromStr, exName, toStr)
+
+	w := tc.Writer(wName)
+
+	// From (example)
+	marshalAndWrite(tc, w, example.Underlying())
+	// To (result)
+	marshalAndWrite(tc, w, result)
+	// Lacunas
+	marshalAndWrite(tc, w, lacunas)
+}
+
+func marshalAndWrite(tc *vanilla.Test, w io.Writer, any interface{}) {
+	tc.Helper()
+
+	bytes, err := json.Marshal(any)
+	require.NoError(tc, err)
+
+	_, err = w.Write(append(bytes, '\n'))
+	require.NoError(tc, err)
 }
