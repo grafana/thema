eaddfcee4b3a8aae62d20512466d84eb9a47a828
Agnès Toulet <35176601+AgnesToulet@users.noreply.github.com>
2023-05-26 12:29:54 +0200

    add tests

--- internal/cuetil/equal.go
+++ internal/cuetil/equal.go
@@ -4,13 +4,42 @@ import (
 	"cuelang.org/go/cue"
 )
 
-// Equal reports nil when the two cue values subsume each other or an error otherwise
-func Equal(val1 cue.Value, val2 cue.Value) error {
-	if err := val1.Subsume(val2, cue.Raw()); err != nil {
+func Subsume(val1 cue.Value, val2 cue.Value) error {
+	err := val1.Subsume(val2, cue.Raw())
+	if err != nil {
+		// cueErr, ok := err.(errors.Error)
+		// if !ok {
+		// 	return err
+		// }
+
+		// errs := errors.Errors(cueErr)
+		// var finalErr errors.Error
+		// for _, e := range errs {
+		// 	msg, args := e.Msg()
+
+		// 	if msg == "invalid value %s (does not satisfy %s)" {
+		// 		if args[0] == args[1] {
+		// 			continue
+		// 		}
+		// 	}
+
+		// 	finalErr = errors.Append(finalErr, e)
+		// }
+
+		// return finalErr
 		return err
 	}
 
-	if err := val2.Subsume(val1, cue.Raw()); err != nil {
+	return nil
+}
+
+// Equal reports nil when the two cue values subsume each other or an error otherwise
+func Equal(val1 cue.Value, val2 cue.Value) error {
+	if err := Subsume(val1, val2); err != nil {
+		return err
+	}
+
+	if err := Subsume(val2, val1); err != nil {
 		return err
 	}
 
